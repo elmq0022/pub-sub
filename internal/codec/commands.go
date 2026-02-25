f1c052adec732254fdac72d486b9fff720d54a88
Aaron Elmquist <aaron.elmquist@protonmail.com>
2026-02-25 00:08:18 -0600

    designates outbound commands

--- internal/codec/commands.go
+++ internal/codec/commands.go
@@ -1,5 +1,11 @@
 package codec
 
+import (
+	"bufio"
+	"errors"
+	"strconv"
+)
+
 type Kind uint8
 
 const (
@@ -18,13 +24,34 @@ type Command interface {
 	Kind() Kind
 }
 
+type OutboundCommands interface {
+	Command
+	EncodeTo(*bufio.Writer) error
+}
+
 type Ping struct{}
 
 func (Ping) Kind() Kind { return KindPing }
 
+func (Ping) EncodeTo(w *bufio.Writer) error {
+	if w == nil {
+		return errors.New("nil writer")
+	}
+	_, err := w.WriteString("PING\r\n")
+	return err
+}
+
 type Pong struct{}
 
 func (Pong) Kind() Kind { return KindPong }
+
+func (Pong) EncodeTo(w *bufio.Writer) error {
+	if w == nil {
+		return errors.New("nil writer")
+	}
+	_, err := w.WriteString("PONG\r\n")
+	return err
+}
 
 type Connect struct{}
 
@@ -61,10 +88,60 @@ type Msg struct {
 
 func (Msg) Kind() Kind { return KindMsg }
 
+func (m Msg) EncodeTo(w *bufio.Writer) error {
+	if w == nil {
+		return errors.New("nil writer")
+	}
+	if len(m.Subject) == 0 {
+		return errors.New("empty subject")
+	}
+	if m.SID < 0 {
+		return errors.New("invalid sid")
+	}
+
+	if _, err := w.WriteString("MSG "); err != nil {
+		return err
+	}
+	if _, err := w.Write(m.Subject); err != nil {
+		return err
+	}
+	if err := w.WriteByte(' '); err != nil {
+		return err
+	}
+	if _, err := w.WriteString(strconv.FormatInt(m.SID, 10)); err != nil {
+		return err
+	}
+	if err := w.WriteByte(' '); err != nil {
+		return err
+	}
+	if _, err := w.WriteString(strconv.Itoa(len(m.Payload))); err != nil {
+		return err
+	}
+	if _, err := w.WriteString("\r\n"); err != nil {
+		return err
+	}
+	if _, err := w.Write(m.Payload); err != nil {
+		return err
+	}
+	if _, err := w.WriteString("\r\n"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // OK is outbound-only and serialized as: +OK\r\n
 type OK struct{}
 
 func (OK) Kind() Kind { return KindOK }
+
+func (OK) EncodeTo(w *bufio.Writer) error {
+	if w == nil {
+		return errors.New("nil writer")
+	}
+	_, err := w.WriteString("+OK\r\n")
+	return err
+}
 
 // Err is outbound-only and serialized as: -ERR <message>\r\n
 type Err struct {
@@ -72,3 +149,17 @@ type Err struct {
 }
 
 func (Err) Kind() Kind { return KindErr }
+
+func (e Err) EncodeTo(w *bufio.Writer) error {
+	if w == nil {
+		return errors.New("nil writer")
+	}
+	if _, err := w.WriteString("-ERR "); err != nil {
+		return err
+	}
+	if _, err := w.WriteString(e.Message); err != nil {
+		return err
+	}
+	_, err := w.WriteString("\r\n")
+	return err
+}
