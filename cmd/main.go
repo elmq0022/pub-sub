6ef9c2295ad1aee47ce5430655ed2db442fbb021
Aaron Elmquist <aaron.elmquist@protonmail.com>
2026-02-26 18:28:35 -0600

    wire up the server - hell yeah!

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/elmq0022/pub-sub/internal/broker"
+	sessioncontroller "github.com/elmq0022/pub-sub/internal/session_controller"
+	"github.com/elmq0022/pub-sub/internal/subjectregistry"
+)
+
+func main() {
+	r := subjectregistry.NewSubjectRegistry()
+	b := broker.NewBroker(r)
+	go b.Run()
+
+	s := sessioncontroller.NewSessionController(b.Input())
+
+	ln, err := net.Listen("tcp", net.JoinHostPort("", "8080"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+
+	fmt.Print("listening on :8080")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println("accept error")
+			continue
+		}
+		s.Start(conn)
+	}
+}
